b96a2405cfc632b35388a7a3ba908acb23cb4830
Polly Bel <polialekseevna80@gmail.com>
2024-10-07 15:24:57 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+n=input("Введите строку на русском ")
+print("Длина строки ",len(n))
+gl=0
+sg=0
+for i in n:
+    if i=="а" or i=="о" or i=="у" or i=="ы" or i=="э" or i=="е" or i=="ё" or i=="ю"  or i=="я" or i=="и":
+        gl+=1
+    else:
+        sg+=1
+print("Количество гласных", gl)
+print("Количество согласных",sg )
+ 
+
