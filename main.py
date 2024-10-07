4a23d57c0fa4b70ac8d5a88b38d8ea6008073888
Polly Bel <polialekseevna80@gmail.com>
2024-10-07 15:51:00 +0300

    main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ sg=0
 for i in n:
     if i=="а" or i=="о" or i=="у" or i=="ы" or i=="э" or i=="е" or i=="ё" or i=="ю"  or i=="я" or i=="и":
         gl+=1
-    else:
+    else i!=" ":
         sg+=1
 print("Количество гласных", gl)
 print("Количество согласных",sg )
